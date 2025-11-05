8526e9b27aa9a38a698d3747d1b37b73a63d5cb8
chubaokhang1611-beep <chubaokhang1611@gmail.com>
2025-11-05 11:27:14 +0700

    Update BusDAO.java

--- src/main/java/vn/edu/schoolname/schoolbusmanagementsystem/dao/BusDAO.java
+++ src/main/java/vn/edu/schoolname/schoolbusmanagementsystem/dao/BusDAO.java
@@ -13,6 +13,25 @@ import java.util.List;
 import vn.edu.schoolname.schoolbusmanagementsystem.model.Bus;
 
 public class BusDAO {
+//khang
+        public List<Bus> getAllBuses() {
+        List<Bus> busList = new ArrayList<>();
+        String sql = "SELECT * FROM buses ORDER BY id DESC";
 
+        try (Connection conn = DBContext.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Bus bus = new Bus();
+                bus.setId(rs.getInt("id"));
+                bus.setLicensePlate(rs.getString("license_plate"));
+                bus.setCapacity(rs.getInt("capacity"));
+                bus.setStatus(rs.getString("status"));
+                busList.add(bus);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return busList;
+    }
     
 }
