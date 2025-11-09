341719f2b98c13ac42612aec52eedf3044e9c7d4
PTDattt <dattienwzw@gmail.com>
2025-11-09 20:29:19 +0700

    MonitorController

--- src/main/java/vn/edu/schoolname/schoolbusmanagementsystem/controller/MonitorController.java
+++ src/main/java/vn/edu/schoolname/schoolbusmanagementsystem/controller/MonitorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import vn.edu.schoolname.schoolbusmanagementsystem.dao.AttendanceDAO;
 import vn.edu.schoolname.schoolbusmanagementsystem.dao.StudentDAO;
 import vn.edu.schoolname.schoolbusmanagementsystem.dao.TripDAO;
+import vn.edu.schoolname.schoolbusmanagementsystem.model.Attendance;
 import vn.edu.schoolname.schoolbusmanagementsystem.model.Student;
 import vn.edu.schoolname.schoolbusmanagementsystem.model.Trip;
 import vn.edu.schoolname.schoolbusmanagementsystem.model.User;
@@ -34,5 +35,55 @@ public class MonitorController {
         return "monitor/dashboard";
     }
 
-    
+    @GetMapping("/trip/{tripId}")
+    public String showAttendancePage(@PathVariable("tripId") int tripId, Model model) {
+        Trip trip = tripDAO.getTripById(tripId);
+        if (trip == null) {
+            return "redirect:/monitor/dashboard";
+        }
+        List<Student> students = studentDAO.getStudentsForTrip(trip.getRoute().getId(), tripId);
+
+        for (Student student : students) {
+            if (student.getRegistration() != null) {
+                Attendance currentTripAttendance = attendanceDAO.getAttendanceStatus(tripId, student.getId());
+
+                student.getRegistration().setAttendanceStatus(currentTripAttendance);
+            }
+        }
+
+        model.addAttribute("trip", trip);
+        model.addAttribute("students", students);
+
+        return "monitor/trip-attendance";
+    }
+
+    @PostMapping("/trip/end")
+    public String endTrip(@RequestParam("tripId") int tripId) {
+        tripDAO.updateTripStatus(tripId, "completed");
+        return "redirect:/monitor/trip/" + tripId;
+    }
+
+    @PostMapping("/trip/check-in")
+    public String handleCheckIn(@RequestParam("tripId") int tripId,
+            @RequestParam("studentId") int studentId,
+            @RequestParam("stopId") int stopId) {
+        attendanceDAO.checkIn(tripId, studentId, stopId);
+        return "redirect:/monitor/trip/" + tripId;
+    }
+
+    @PostMapping("/trip/check-out")
+    public String handleCheckOut(@RequestParam("tripId") int tripId,
+            @RequestParam("studentId") int studentId,
+            @RequestParam("stopId") int stopId) {
+
+        attendanceDAO.checkOut(tripId, studentId, stopId);
+
+        return "redirect:/monitor/trip/" + tripId;
+    }
+
+    @PostMapping("/trip/start")
+    public String startTrip(@RequestParam("tripId") int tripId) {
+        tripDAO.updateTripStatus(tripId, "ongoing");
+        return "redirect:/monitor/trip/" + tripId;
+    }
 }
